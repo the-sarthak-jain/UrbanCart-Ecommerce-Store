4ce33935fc4703a5b8478c7a47a33c6b6312fc19
Sarthak Jain <112318814+the-sarthak-jain@users.noreply.github.com>
2024-08-24 18:56:49 +0530

    Initial commit

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,8 +1,22 @@
 import productModel from "../models/productModel.js";
 import categoryModel from "../models/categorymodel.js";
+import orderModel from "../models/orderModel.js";
 
 import fs from "fs";
 import slugify from "slugify";
+import braintree from "braintree";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+dotenv.config();
+
+// payment Gateway:
+var gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
 
 export const createProductController = async (req, res) => {
   try {
@@ -273,5 +287,55 @@ export const searchProductController = async (req, res) => {
       message: "Error in Searching Products",
       error,
     });
+  }
+};
+
+// Payment Gateway Api
+// for token
+export const braintreeTokenController = async (req, res) => {
+  try {
+    gateway.clientToken.generate({}, function (err, response) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// for payment
+export const braintreePaymentController = async (req, res) => {
+  try {
+    const { nonce, cart } = req.body;
+    let total = 0;
+    cart.map((i) => {
+      total += i.price;
+    });
+    let newTransaction = gateway.transaction.sale(
+      {
+        amount: total,
+        paymentMethodNonce: nonce,
+        options: {
+          submitForSettlement: true,
+        },
+      },
+      function (error, result) {
+        if (result) {
+          const order = new orderModel({
+            products: cart,
+            payment: result,
+            buyer: req.user._id,
+          }).save();
+          res.json({ ok: true });
+        } else {
+          res.status(500).send(error);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
 };
